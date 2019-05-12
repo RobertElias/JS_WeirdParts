3c1db5a1f31c537cd1e4b5b8df58c0f142ce2133
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-11 21:53:36 -0700

    Updates

--- Objects/Objects_Dot.js
+++ Objects/Objects_Dot.js
@@ -1,4 +1,22 @@
 var person = new Object();
 
 person["firstname"] = "Robert";
+person["lasttname"] = "Elias";
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]); 
+
+console.log(person.firstname);
+console.log(person.lasttname);
+
+person.address = new Object();
+person.address.street = "111 Main St.";
+person.address.city = "Orange";
+person.address.state = "CA";
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
 
