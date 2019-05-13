3d2f9bd0dbd7d237bc93baabab304998a1119d48
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-12 19:24:19 -0700

    Update

--- INTRO/app.js
+++ INTRO/app.js
@@ -25,18 +25,30 @@
 
 //Variable Environment: Where is the variable
 
+// function b(){
+//     var myVar;
+//     console.log(myVar);
+// }
+
+// function a() {
+//     var myVar = 2;
+//     console.log(myVar);
+//     b();
+// }
+
+// var myVar = 1;
+// console.log(myVar);
+// a();
+// console.log(myVar);
+
 function b(){
-    var myVar;
     console.log(myVar);
 }
 
 function a() {
     var myVar = 2;
-    console.log(myVar);
     b();
 }
 
 var myVar = 1;
-console.log(myVar);
 a();
-console.log(myVar);
