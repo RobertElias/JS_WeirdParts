6ca9e10449bf5829cb1d1c82965ad4df3adef3c0
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-12 19:18:40 -0700

    Updates

--- INTRO/app.js
+++ INTRO/app.js
@@ -27,12 +27,15 @@ var d;
 
 function b(){
     var myVar;
+    console.log(myVar);
 }
 
 function a() {
     var myVar = 2;
+    console.log(myVar);
     b();
 }
 
 var myVar = 1;
+console.log(myVar);
 a();
