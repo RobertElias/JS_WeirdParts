8baa0b86085a4fab55cb2b6a8c02bed9461b4659
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-12 19:20:49 -0700

    Updates

--- INTRO/app.js
+++ INTRO/app.js
@@ -1,27 +1,27 @@
-function b(){
-    console.log('Called b!')
-}
- b();
-console.log(a)
+// function b(){
+//     console.log('Called b!')
+// }
+//  b();
+// console.log(a)
 
-var a = 'Hello World!';
+// var a = 'Hello World!';
 
-console.log(a);
-//////////////////////
+// console.log(a);
+// //////////////////////
 
-//invocation
+// //invocation
 
-function a(){
-    b();
-    var c;
-}
+// function a(){
+//     b();
+//     var c;
+// }
 
-function b(){
-    var d;
-}
+// function b(){
+//     var d;
+// }
 
-a();
-var d;
+// a();
+// var d;
 
 //Variable Environment: Where is the variable
 
@@ -39,3 +39,4 @@ function a() {
 var myVar = 1;
 console.log(myVar);
 a();
+console.log(myVar);
