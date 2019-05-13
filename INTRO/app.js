cbb5599fe25a7998cf37009036222a47fa3a90df
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-12 19:13:04 -0700

    Updates

--- INTRO/app.js
+++ INTRO/app.js
@@ -7,3 +7,18 @@ console.log(a)
 var a = 'Hello World!';
 
 console.log(a);
+//////////////////////
+
+//invocation
+
+function a(){
+    b();
+    var c;
+}
+
+function b(){
+    var d;
+}
+
+a();
+var d;
